8f84bccb043a6c45690a25d480bfcfde547ab5ae
Huyen Vu <huyenvu2210@gmail.com>
2021-10-22 23:47:14 +0700

    create a form at /

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+
+    if (url === '/') {
+        res.setHeader('Content-type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My first page</title></head>');
+        res.write('<body><form method="post" action="/message"><input type="text" name="message"/><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
     res.setHeader('Content-type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My first page</title></head>');
     res.write('<body><h1>Hello from my Node.js server</h1></body>');
     res.write('</html>');
-    res.end()
+    res.end();
 });
 
 server.listen(3000);
