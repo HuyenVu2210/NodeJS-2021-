403a9793251c4e240c1e4fe52c0fee5afb1c6ea1
Huyen Vu <huyenvu2210@gmail.com>
2021-10-23 15:37:09 +0700

    create a txt file with DUMMY text in it

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
 
     if (url === '/') {
         res.setHeader('Content-type', 'text/html');
         res.write('<html>');
         res.write('<head><title>My first page</title></head>');
-        res.write('<body><form method="post" action="/message"><input type="text" name="message"/><button type="submit">Send</button></form></body>');
+        res.write('<body><form method="POST" action="/message"><input type="text" name="message"/><button type="submit">Send</button></form></body>');
         res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
 
