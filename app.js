93825c1b21a0ef9e9434830360266a0ba7e02d55
Huyen Vu <huyenvu2210@gmail.com>
2021-10-24 19:08:47 +0700

    send a h1 response

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-    console.log("This is another middleware")
+    console.log("This is another middleware");
+    res.send('<h1>Hello from Express.js</h1>')
 })
 
 const server = http.createServer(app);
