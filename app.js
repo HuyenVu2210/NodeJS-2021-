a406afa457bf3ea0049039942519457993fd4aef
Huyen Vu <huyenvu2210@gmail.com>
2021-10-24 19:21:11 +0700

    add 3 middlewares for different routes

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("This is a middleware");
+app.use('/', (req, res, next) => {
+    console.log('This always run!');
     next();
 });
 
-app.use((req, res, next) => {
-    console.log("This is another middleware");
-    res.send('<h1>Hello from Express.js</h1>')
+app.use('/add-product', (req, res, next) => {
+    res.send('<h1>This is the add-product page!</h1>');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from Express.Js!</h1>');
 })
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
