c0e15db6ff7bf83fd34dc23a17a55e21f8eddb94
Huyen Vu <huyenvu2210@gmail.com>
2021-10-25 23:00:58 +0700

    add form to add-product page

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', (req, res, next) => {
     console.log('This always run!');
@@ -8,8 +11,13 @@ app.use('/', (req, res, next) => {
 });
 
 app.use('/add-product', (req, res, next) => {
-    res.send('<h1>This is the add-product page!</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add</button></form>');
 });
+
+app.use("/product", (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/')
+})
 
 app.use('/', (req, res, next) => {
     res.send('<h1>Hello from Express.Js!</h1>');
