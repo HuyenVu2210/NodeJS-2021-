fab4bcc048881f6792ce4b7ce211d11570abde1e
Huyen Vu <huyenvu2210@gmail.com>
2021-10-26 22:51:25 +0700

    add 404.pug & add-product.pug

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use('/', (req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+    res.render('404', { docTitle: 'Page not found' })
 })
 
 app.listen(3000);
