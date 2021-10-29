03c1911d86f6406469adede8525b36954daaeb1e
Huyen Vu <huyenvu2210@gmail.com>
2021-10-29 21:18:53 +0700

    use query to get data from the url

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Products = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("admin/add-product", {
+  res.render("admin/edit-product", {
     docTitle: "Add Product",
     path: "/admin/add-product",
   });
@@ -15,6 +15,18 @@ exports.postAddProduct = (req, res, next) => {
   const products = new Products(title, imageUrl, description, price);
   products.save();
   res.redirect("/products");
+};
+
+exports.getEditProducts = (req, res, next) => {
+  const editMode = req.query.edit;
+  if(!editMode) {
+    return res.redirect('/')
+  }
+  res.render("admin/edit-product", {
+    docTitle: "Edit Product",
+    path: "/admin/edit-product",
+    editing: editMode
+  });
 };
 
 exports.getProducts = (req, res, next) => {
