27f9ff71c19a4333ab14d48fda8cb5f3d03472b5
Huyen Vu <huyenvu2210@gmail.com>
2021-10-31 14:38:54 +0700

    delete product from cart

--- models/product.js
+++ models/product.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const Cart = require("./cart");
 
 const p = path.join(
     path.dirname(process.mainModule.filename),
@@ -70,9 +71,13 @@ module.exports = class Product {
 
   static deleteById = (id) => {
     getProductsFromFile((products) => {
+      const product = products.find(prod => prod.id === id);
       const updatedProducts = products.filter(prod => prod.id !== id);
       fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-        console.log(err + ' delete failed');
+        if (!err) {
+          // also delete from cart when product is deleted
+          Cart.deleteById(id, product.price)
+        }
       });
     });
   }
