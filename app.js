aedbbc949f4fbca5a9630ef3e5489b76782f7070
Huyen Vu <huyenvu2210@gmail.com>
2021-11-02 21:29:45 +0700

    create id 1 dummy user

--- app.js
+++ app.js
@@ -12,11 +12,24 @@ const errorsController = require("./controllers/errors");
 
 // Database
 const sequelize = require("./util/database");
+const Product = require('./models/product');
+const User = require('./models/user');
 
 // Create app
 const app = express();
 
 app.set("view engine", "ejs");
+
+// Store user in request
+app.use((req, res, next) => {
+  User.findByPk(1).then(user => {
+    req.user = user;
+    next();
+  })
+  .catch(err => {
+    console.log(err)
+  })
+})
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -27,10 +40,28 @@ app.use(shopRoutes);
 
 app.use("/", errorsController.get404);
 
+// Create relationship for models
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
 sequelize
   .sync()
   .then((results) => {
     // console.log(results);
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      User.create({
+        id: '1',
+        name: 'Huyen beo',
+        email: 'huyenbeo@gmail.com'
+      });
+    }
+    return user
+  })
+  .then(user => {
+    console.log(user);
     app.listen(3000);
   })
   .catch((err) => {
--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const sequelize = require('../util/database');
 const User = sequelize.define('user', {
     id: {
         type: Sequelize.INTEGER,
+        autoIncrement: true,
         allowNull: false,
         primaryKey: true
     },
