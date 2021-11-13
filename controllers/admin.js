9b87f330babbd8e855c09ec8c1dda26e313abc38
Huyen Vu <huyenvu2210@gmail.com>
2021-11-13 23:46:38 +0700

    subtract dayoff from annualLeave && not allow adding new dayoff if annualLeave is not enough

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,8 @@
 // const Product = require('../models/product');
-const Checkin = require('../models/checkin');
-const Dayoff = require('../models/dayoff');
-const Timesheet = require('../models/timesheet');
-const url = require('url'); 
+const Checkin = require("../models/checkin");
+const Dayoff = require("../models/dayoff");
+const Timesheet = require("../models/timesheet");
+const url = require("url");
 // const User = require("../models/user");
 
 // const mongodb = require("mongodb");
@@ -81,42 +81,44 @@ exports.getCheckIn = (req, res, next) => {
   const Staff = req.staff;
   let isCheckedIn = false;
   let cannot = req.query.cannot;
-  let noTimesheet = req.query.noTimesheet
+  let noTimesheet = req.query.noTimesheet;
+  let overLeave = req.query.overLeave;
 
-  console.log(cannot)
-  Checkin.find({'staffId': req.staff._id, end: null}).then(checkin => {
-    if (checkin.length > 0) {
-      isCheckedIn = true;
-    }
-    res.render('check-in', {
-      staff: Staff,
-      docTitle: Staff.name,
-      path: "/",
-      isCheckedIn: isCheckedIn,
-      cannot: cannot,
-      noTimesheet: noTimesheet
+  console.log(cannot);
+  Checkin.find({ staffId: req.staff._id, end: null })
+    .then((checkin) => {
+      if (checkin.length > 0) {
+        isCheckedIn = true;
+      }
+      res.render("check-in", {
+        staff: Staff,
+        docTitle: Staff.name,
+        path: "/",
+        isCheckedIn: isCheckedIn,
+        cannot: cannot,
+        noTimesheet: noTimesheet,
+        overLeave: overLeave
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  })
-  .catch(err => {
-    console.log(err)
-  })
 };
 
-// post checkin 
+// post checkin
 exports.postCheckIn = (req, res, next) => {
-  Checkin.find({'staffId': req.staff._id, end: null})
-  .then(c => {
+  Checkin.find({ staffId: req.staff._id, end: null }).then((c) => {
     if (c.length > 0) {
       let existingCheckin = c[0];
-      const checkout_time = new Date;
+      const checkout_time = new Date();
       existingCheckin.end = checkout_time;
       let hour =
-        ((checkout_time.getHours() +
-        checkout_time.getMinutes() / 60) -
+        checkout_time.getHours() +
+        checkout_time.getMinutes() / 60 -
         (existingCheckin.start.getHours() +
-          existingCheckin.start.getMinutes() / 60));
-      
-      existingCheckin.hour = Math.round(hour*100)/100;
+          existingCheckin.start.getMinutes() / 60);
+
+      existingCheckin.hour = Math.round(hour * 100) / 100;
 
       console.log(typeof hour);
       existingCheckin
@@ -125,54 +127,57 @@ exports.postCheckIn = (req, res, next) => {
           console.log("checked in/out");
           Checkin.aggregate(
             [
-              { $match: { 'staffId': req.staff._id } },
+              { $match: { staffId: req.staff._id } },
               {
                 $group: {
                   _id: { $dateToString: { format: "%Y-%m-%d", date: "$date" } },
                   totalHours: {
-                    $sum: "$hour"
-                  }
-                }
+                    $sum: "$hour",
+                  },
+                },
               },
-              { $sort: { _id: -1 } }
-            ], 
-            function(err, results) {
+              { $sort: { _id: -1 } },
+            ],
+            function (err, results) {
               if (err) {
                 console.log(err);
               } else {
                 // console.log(results);
                 let forRenderTimesheet;
-                time = results.map(i => {
-                  const date = i._id + 'T00:00:00.000+00:00';
+                time = results.map((i) => {
+                  const date = i._id + "T00:00:00.000+00:00";
                   const totalHours = i.totalHours;
                   const overTime = totalHours > 8 ? totalHours - 8 : 0;
-                  return Checkin.find({'date': date})     // have to return in able to handle as a promise
-                  .then(checkin => {
-                    return { _id: date, checkin: checkin, totalHours: totalHours, overTime: overTime }
-                  });
+                  return Checkin.find({ date: date }) // have to return in able to handle as a promise
+                    .then((checkin) => {
+                      return {
+                        _id: date,
+                        checkin: checkin,
+                        totalHours: totalHours,
+                        overTime: overTime,
+                      };
+                    });
                 });
-        
-                Promise.all(time).then(function(results) {
+
+                Promise.all(time).then(function (results) {
                   forRenderTimesheet = results;
                   console.log(JSON.stringify(forRenderTimesheet));
-        
-                  Timesheet.find({'staffId': req.staff._id})
-                  .then(t => {
-        
+
+                  Timesheet.find({ staffId: req.staff._id }).then((t) => {
                     // create temporary timesheet to get info
                     const timesheet = new Timesheet({
                       staffId: req.staff._id,
-                      timesheet: []
+                      timesheet: [],
                     });
-                    forRenderTimesheet.forEach(i => {
+                    forRenderTimesheet.forEach((i) => {
                       timesheet.timesheet.push({
-                          _id: i._id,
-                          checkin: [...i.checkin],
-                          totalHours: i.totalHours,
-                          overTime: i.overTime,
-                        });
-                    }); 
-        
+                        _id: i._id,
+                        checkin: [...i.checkin],
+                        totalHours: i.totalHours,
+                        overTime: i.overTime,
+                      });
+                    });
+
                     if (t.length > 0) {
                       let existingTimesheet = t[0];
                       existingTimesheet.timesheet = timesheet.timesheet;
@@ -184,152 +189,181 @@ exports.postCheckIn = (req, res, next) => {
                         res.redirect("/");
                       });
                     }
-                  })       
-                })
+                  });
+                });
               }
-            })
+            }
+          );
         })
         .catch((err) => {
           console.log("post checkin failed: " + err);
         });
     } else {
-      let checkin = new Checkin;
+      let checkin = new Checkin();
       const workplace = req.body.workplace;
       const today = new Date();
-      const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+      const date =
+        today.getFullYear() +
+        "-" +
+        (today.getMonth() + 1) +
+        "-" +
+        today.getDate();
       console.log(date);
 
       checkin.workplace = workplace;
       checkin.start = today;
       checkin.date = date;
       checkin.staffId = req.staff._id;
-      checkin.save()
-      .then((results) => {
-        console.log("checked in/out");
-        res.redirect("/");
-      })
-      .catch((err) => {
-        console.log("post checkin failed: " + err);
-      });
+      checkin
+        .save()
+        .then((results) => {
+          console.log("checked in/out");
+          res.redirect("/");
+        })
+        .catch((err) => {
+          console.log("post checkin failed: " + err);
+        });
     }
-  })
+  });
 };
 
 // get timesheet
 exports.getTimesheet = (req, res, next) => {
-  Timesheet.find({'staffId': req.staff._id})
-  .then(t => {
+  Timesheet.find({ staffId: req.staff._id }).then((t) => {
     if (t.length > 0) {
       const timesheet = t[0];
 
       let result = timesheet.timesheet.reduce(function (t, a) {
-          t[a._id.slice(5,7)] = t[a._id.slice(5,7)] || [];
-          t[a._id.slice(5,7)].push(a);
-          return t;
-        }, Object.create(null));
+        t[a._id.slice(5, 7)] = t[a._id.slice(5, 7)] || [];
+        t[a._id.slice(5, 7)].push(a);
+        return t;
+      }, Object.create(null));
 
       // console.log(JSON.stringify(result));
 
-
-      res.render('timesheet', {
+      res.render("timesheet", {
         staff: req.staff,
         docTitle: req.staff.name,
         path: "/timesheet",
         timesheet: timesheet.timesheet,
-        months: result
-      })
+        months: result,
+      });
     } else {
-      res.redirect(url.format({
-        pathname:"/",
-        query: {
-           noTimesheet: true
-         }
-      }))
+      res.redirect(
+        url.format({
+          pathname: "/",
+          query: {
+            noTimesheet: true,
+          },
+        })
+      );
     }
-  })
+  });
 };
 
 // get covid info form
 exports.getVaccine = (req, res, next) => {
   const Staff = req.staff;
-  res.render('vaccine', {
+  res.render("vaccine", {
     staff: Staff,
     docTitle: Staff.name,
     path: "/vaccine",
   });
 };
 
-// post covid info 
+// post covid info
 exports.postVaccine = (req, res, next) => {
-    const tem = req.body.tem;
-    const shot1 = req.body.shot1;
-    const newDate = new Date();
-    const newDate1 = newDate.toISOString().slice(0,10) + 'T00:00:00.000+00:00';
+  const tem = req.body.tem;
+  const shot1 = req.body.shot1;
+  const newDate = new Date();
+  const newDate1 = newDate.toISOString().slice(0, 10) + "T00:00:00.000+00:00";
 
-    const date1 = req.body.date1 === '' ? newDate1 : req.body.date1;
-    const shot2 = req.body.shot2;
-    const date2 = req.body.date2 === '' ? newDate1 : req.body.date2;
-    const result = req.body.result;
+  const date1 = req.body.date1 === "" ? newDate1 : req.body.date1;
+  const shot2 = req.body.shot2;
+  const date2 = req.body.date2 === "" ? newDate1 : req.body.date2;
+  const result = req.body.result;
 
-    const v1 = {shot: shot1, date: date1};
-    const v2 = {shot: shot2, date: date2};
+  const v1 = { shot: shot1, date: date1 };
+  const v2 = { shot: shot2, date: date2 };
 
-    req.staff.covid.tem = tem;
-    req.staff.covid.result = result;
-    req.staff.covid.vaccine[0] = v1;
-    req.staff.covid.vaccine[1] = v2;
+  req.staff.covid.tem = tem;
+  req.staff.covid.result = result;
+  req.staff.covid.vaccine[0] = v1;
+  req.staff.covid.vaccine[1] = v2;
 
-    req.staff.save()
-    .then(results => {
-      res.redirect('/')
-    })
+  req.staff.save().then((results) => {
+    res.redirect('/');
+  });
 };
 
-// post dayoff info 
+// post dayoff info
 exports.postDayoff = (req, res, next) => {
-  const reqdayoff = req.body.dayoff + 'T00:00:00.000+00:00';
-  const houroff = Math.round(req.body.houroff*100)/100;
+  const reqdayoff = req.body.dayoff + "T00:00:00.000+00:00";
+  const houroff = Math.round(req.body.houroff * 100) / 100;
 
-  Dayoff.find({'staffId': req.staff._id, 'date': reqdayoff})
-  .then(dayoff => {
-    if (dayoff.length > 0) {
-      let existingDayoff = dayoff[0];
-      let existingHoursOff = existingDayoff.totalHoursOff;
-      const totalHoursOff = existingHoursOff + houroff < 8 ? existingHoursOff + houroff: existingHoursOff;
-      existingDayoff.totalHoursOff = totalHoursOff;
-
-      const cannot = totalHoursOff !== existingHoursOff + houroff;
-      
-        existingDayoff.save()
-        .then(results => {
-          res.redirect(url.format({
-            pathname:"/",
-            query: {
-               cannot: cannot
-             }
-          }));
-        })
-    } else {
-      const newDayoff = new Dayoff({
-        staffId: req.staff._id,
-        date: reqdayoff,
-        totalHoursOff: houroff
-      });
-      newDayoff.save()
-      .then(results => {
-        res.redirect('/');
+  if (req.staff.annualLeave - houroff < 0) {
+    res.redirect(
+      url.format({
+        pathname: "/",
+        query: {
+          overLeave: true,
+        },
       })
-    }
-  })
+    );
+  } else {
+    Dayoff.find({ staffId: req.staff._id, date: reqdayoff }).then((dayoff) => {
+      if (dayoff.length > 0) {
+        let existingDayoff = dayoff[0];
+        let existingHoursOff = existingDayoff.totalHoursOff;
+        const totalHoursOff =
+          existingHoursOff + houroff < 8
+            ? existingHoursOff + houroff
+            : existingHoursOff;
+        existingDayoff.totalHoursOff = totalHoursOff;
+  
+        const cannot = totalHoursOff !== existingHoursOff + houroff;
+  
+        existingDayoff.save().then((results) => {
+          req.staff.annualLeave =
+            totalHoursOff !== existingHoursOff + houroff
+              ? req.staff.annualLeave
+              : req.staff.annualLeave - totalHoursOff;
+  
+          req.staff.save()
+          .then(results => {
+            res.redirect(
+              url.format({
+                pathname: "/",
+                query: {
+                  cannot: cannot,
+                },
+              })
+            );
+          })
+        });
+      } else {
+        const newDayoff = new Dayoff({
+          staffId: req.staff._id,
+          date: reqdayoff,
+          totalHoursOff: houroff,
+        });
+        newDayoff.save().then((results) => {
+          req.staff.annualLeave = req.staff.annualLeave - houroff;
+          req.staff.save()
+          .then(results => {
+            res.redirect("/");
+          })
+        });
+      }
+    });
+  }
 };
-
 
 // get salary
 exports.getSalary = (req, res, next) => {
   const month = req.params.month;
   console.log(month);
-  Timesheet.find({'staffId': req.staff._id})
-  .then(t => {
+  Timesheet.find({ staffId: req.staff._id }).then((t) => {
     if (t.length > 0) {
       const timesheet = t[0];
 
@@ -343,11 +377,10 @@ exports.getSalary = (req, res, next) => {
         if (key === month) {
           let overtime = 0;
           // console.log(value)
-          value.forEach(v => {
+          value.forEach((v) => {
             overtime = overtime + v.overTime;
-          })
+          });
           console.log(overtime);
-          
         }
       }
       // res.render('timesheet', {
@@ -365,10 +398,8 @@ exports.getSalary = (req, res, next) => {
       //    }
       // }))
     }
-  })
+  });
 };
-
-
 
 // delete product
 // exports.postDeleteProduct = (req, res, next) => {
@@ -384,4 +415,3 @@ exports.getSalary = (req, res, next) => {
 // };
 
 // get all products ==> /admin/products
-
